c5e0f11ecf115af017da10a00bd69bc23c9755ea
Tchamde Djeumo Yves <tchamdeyves@gmail.com>
2022-07-16 09:55:57 +0200

    Add Roles controller and index view role

---
+++ IdentityProject/Controllers/RolesController.cs
@@ -0,0 +1,29 @@
+﻿using IdentityProject.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityProject.Controllers
+{
+    public class RolesController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(UserManager<IdentityUser> userManager, ApplicationDbContext context, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _context = context;
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var rolesList = _context.Roles.ToList();
+            return View(rolesList);
+        }
+    }
+}
