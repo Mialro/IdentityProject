a779486c4d721c21f0e58202f8cc2924109cd7ec
Tchamde Djeumo Yves <tchamdeyves@gmail.com>
2022-07-16 09:27:21 +0200

    User Controller

---
+++ IdentityProject/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using IdentityProject.Data;
+using IdentityProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UserController(UserManager<IdentityUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var userList = _context.ApplicationUser.ToList();
+            var userRoles = _context.UserRoles.ToList();
+            var roles = _context.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var role = userRoles.FirstOrDefault(x => x.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None Role";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(x => x.Id == role.RoleId).Name;
+
+                }
+            }
+            return View(userList);
+        }
+    }
+}
